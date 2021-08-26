d74a284525cb4aa6cc24a2078210fc67df30b8bb
MrManu <superbrandon2018@gmail.com>
2021-08-25 19:20:15 -0500

    Fixed Cors and Meta-etiquetas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Head from 'next/head'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
@@ -17,6 +18,12 @@ const HomePage = () => {
 
   return (
     <Layout>
+      <Head>
+        <title>Avocados</title>
+        <meta property="og:title" content="Platzi Avocado" />
+        <meta name="description" property='og:description' content="Pagina de Avocados con NextJS" />
+        <meta name="description" property='og:image' content="/images/maluma.jpg" />
+      </Head>
       <KawaiiHeader />
       <ProductList products={productList} />
     </Layout>
